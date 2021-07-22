4e00c1865a9320c6072c207c4960ec9963834dfb
Taniya0501 <iit2019054@iiita.ac.in>
2021-07-22 16:03:32 -0700

    ejs-mate tool added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -17,6 +18,7 @@ db.once('open', ()=> {
 	console.log('Database Connected');
 });
 
+app.engine('ejs',ejsMate);
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
