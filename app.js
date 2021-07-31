3c0faaab33a72ed7c3f92255c12394a5aef64316
Taniya0501 <iit2019054@iiita.ac.in>
2021-07-31 13:37:13 -0700

    redirect path issue resolved

--- app.js
+++ app.js
@@ -115,10 +115,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-	// if(!['/login','/'].includes(req.originalUrl)) {
-	// 	req.session.returnTo = req.originalUrl;
-	// 	console.log(req.originalUrl);
-	// }
+	if(!['/login','/'].includes(req.originalUrl)) {
+		req.session.returnTo = req.originalUrl;
+		// console.log(req.originalUrl);
+	}
 	res.locals.currentUser = req.user;
 	res.locals.success = req.flash('success');
 	res.locals.error = req.flash('error');
